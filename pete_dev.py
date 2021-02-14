2dd93b580f292659feadf4b233065954df12b3ad
Pete Green <p.l.green@liverpool.ac.uk>
2021-02-14 13:34:22 +0000

    now accounts for the case where there are no moves left

--- pete_dev.py
+++ pete_dev.py
@@ -3,33 +3,39 @@ import numpy as np
 from numpy.random import randint
 
 """
-Some random players playing against each other, so Pete can 
-understand what's going on. 
+Some random players playing against each other, so Pete can
+understand what's going on.
 
 """
 
-# Initialise with a blank board
-board = ttt.initial_state()
+for n_MC in range(10):
 
-# Used to store the winner
-w = None
+    # Initialise with a blank board
+    board = ttt.initial_state()
 
-# Play until there's a winner
-while w is None:
-
-    # List all possible moves
+    # Initial moves
     moves = list(ttt.actions(board))
-    
-    # Pick a random move
-    n = randint(low=0, high=len(moves))
-    action = moves[n]
-    
-    # Update the board (result automatically checks
-    # whose go it is)
-    board = ttt.result(board, action)
-    
-    # See if anyone has won 
-    w = ttt.winner(board)
 
-print('Winner is ', w, '\n')
-[print(i) for i in board]
+    # Used to store the winner
+    w = None
+
+    # Play until there's a winner
+    while w is None or len(moves) > 0:
+
+        # Pick a random move
+        n = randint(low=0, high=len(moves))
+        action = moves[n]
+
+        # Update the board (result automatically checks
+        # whose go it is)
+        board = ttt.result(board, action)
+        
+        # List all possible moves
+        moves = list(ttt.actions(board))
+
+        # See if anyone has won
+        w = ttt.winner(board)
+
+
+    print('Winner is ', w, '\n')
+##[print(i) for i in board]
