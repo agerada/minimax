de12dc27675cb773ed2861bc99c3fcc6b1bfd3fe
Pete Green <p.l.green@liverpool.ac.uk>
2021-02-14 13:38:22 +0000

    now breaks if we run out of moves

--- pete_dev.py
+++ pete_dev.py
@@ -8,34 +8,35 @@ understand what's going on.
 
 """
 
-for n_MC in range(10):
+# Initialise with a blank board
+board = ttt.initial_state()
 
-    # Initialise with a blank board
-    board = ttt.initial_state()
+# Initial moves
+moves = list(ttt.actions(board))
 
-    # Initial moves
+# Used to store the winner
+w = None
+
+# Play until there's a winner
+while w is None:
+
+    # Pick a random move
+    n = randint(low=0, high=len(moves))
+    action = moves[n]
+
+    # Update the board (result automatically checks
+    # whose go it is)
+    board = ttt.result(board, action)
+    
+    # List all possible moves
     moves = list(ttt.actions(board))
 
-    # Used to store the winner
-    w = None
+    # See if anyone has won
+    w = ttt.winner(board)
+    
+    # Break if we've run out of moves
+    if len(moves) == 0:
+        break
 
-    # Play until there's a winner
-    while w is None or len(moves) > 0:
-
-        # Pick a random move
-        n = randint(low=0, high=len(moves))
-        action = moves[n]
-
-        # Update the board (result automatically checks
-        # whose go it is)
-        board = ttt.result(board, action)
-        
-        # List all possible moves
-        moves = list(ttt.actions(board))
-
-        # See if anyone has won
-        w = ttt.winner(board)
-
-
-    print('Winner is ', w, '\n')
-##[print(i) for i in board]
+print('Winner is ', w, '\n')
+[print(i) for i in board]
