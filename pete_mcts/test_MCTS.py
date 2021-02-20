3c1d3952a1d36b35a34b3239a6efb27e0ce453f0
Pete Green <p.l.green@liverpool.ac.uk>
2021-02-20 16:45:36 +0000

    Testing updated to take state as tuple

--- pete_mcts/test_MCTS.py
+++ pete_mcts/test_MCTS.py
@@ -29,14 +29,13 @@ mcts = MCTS()
 
 # Define simple game for testing purposes
 def find_children(node):
-
-    if node == 0:
-        children = [1, 2]
-    if node == 1:
-        children = [3, 4]
-    if node == 2:
-        children = [5, 6]
-    if node in [3, 4, 5, 6]:
+    if node == (0,):
+        children = [(1,), (2,)]
+    if node == (1,):
+        children = [(3,), (4,)]
+    if node == (2,):
+        children = [(5,), (6,)]
+    if node in [(3,), (4,), (5,), (6,)]:
         children = None
 
     return children
@@ -61,23 +60,21 @@ def random_sim(node):
 
 def find_reward(node):
 
-    if node == 3:
+    if node == (3,):
         reward = 1
-    if node == 4:
+    if node == (4,):
         reward = 2
-    if node == 5:
+    if node == (5,):
         reward = 0
-    if node == 6:
+    if node == (6,):
         reward = 1
 
     return reward
-
 
 # Assign rules of the game to mcts instance
 mcts.find_children = find_children
 mcts.random_sim = random_sim
 mcts.find_reward = find_reward
-
 
 def test_itt1():
 
@@ -86,49 +83,58 @@ def test_itt1():
 
     # Run first iteration and check that the nodes dictionaries are
     # what we would expect.
-    mcts.run_itt(node=0)
-    assert mcts.nodes_and_chldn == dict({0: [1, 2]})
-    assert mcts.N == dict({0: 1, 4: 1})
-    assert mcts.rewards == dict({0: 2, 4: 2})
+    mcts.run_itt(node=(0,))
+    assert mcts.nodes_and_chldn == dict({(0,): [(1,), (2,)]})
+    assert mcts.N == dict({(0,): 1, (4,): 1})
+    assert mcts.rewards == dict({(0,): 2, (4,): 2})
 
 
 def test_itt2():
     # On the second iteration (with random seed fixed) it should rollout
-    # from node 1. This means that nodes 0 and 1 have been visited twice and
+    # from node 2. This means that nodes 0 and 2 have been visited twice and
     # once respectively.
-    mcts.run_itt(node=0)
-    assert mcts.nodes_and_chldn == {0: [1, 2], 1: [3, 4]}
-    assert mcts.N == {0: 2, 4: 1, 1: 1, 3: 1}
+
+    mcts.run_itt(node=(0,))
+    assert mcts.nodes_and_chldn == {(0,): [(1,), (2,)],
+                                    (2,): [(5,), (6,)]}
+    assert mcts.N == {(0,): 2, (4,): 1, (2,): 1, (5,): 1}
 
 
 def test_itt3():
     # On the third iteration (with random seed fixed) rollout has to be
-    # conducted from node 2.
-    mcts.run_itt(node=0)
-    assert mcts.nodes_and_chldn == {0: [1, 2], 1: [3, 4], 2: [5, 6]}
+    # conducted from node 1.
+
+    mcts.run_itt(node=(0,))
+    assert mcts.nodes_and_chldn == {(0,): [(1,), (2,)],
+                                    (2,): [(5,), (6,)],
+                                    (1,): [(3,), (4,)]}
 
 
 def test_itt4():
     # On the fourth iteration we should pick the node with the largest
     # uct value and then rollout from one of its children. We are now hitting
     # leaf nodes so the dictionary of visited nodes should remain the same.
-    mcts.run_itt(node=0)
-    assert mcts.nodes_and_chldn == {0: [1, 2], 1: [3, 4], 2: [5, 6]}
+
+    mcts.run_itt(node=(0,))
+    assert mcts.nodes_and_chldn == {(0,): [(1,), (2,)],
+                                    (2,): [(5,), (6,)],
+                                    (1,): [(3,), (4,)]}
 
 
 def test_monte_calro():
     # Now if we run it lots of times, the expected reward associated with
     # node 1 should converge to 1.5
+
     for i in range(1000):
-        mcts.run_itt(node=0)
-    assert np.allclose(mcts.rewards[1] / mcts.N[1], 1.5, atol=0.01)
+        mcts.run_itt(node=(0,))
+    assert np.allclose(mcts.rewards[1,] / mcts.N[1,], 1.5, atol=0.01)
 
     # Check that it chooses node 1 on the first move
-    best_node = mcts.choose_move(node=0)
-    assert best_node == 1
+    best_node = mcts.choose_move(node=(0,))
+    assert best_node == (1,)
 
     # Check that it chooses node 4 on the second move
     for i in range(1000):
         mcts.run_itt(node=best_node)
     best_node = mcts.choose_move(node=best_node)
-    assert best_node == 4
+    assert best_node == (4,)
