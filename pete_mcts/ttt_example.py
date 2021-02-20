666951cca24bfb5e41a0c977a1f609ffc7e14fad
Pete Green <p.l.green@liverpool.ac.uk>
2021-02-20 17:10:52 +0000

    Need to look at classes for nodes

--- pete_mcts/ttt_example.py
+++ pete_mcts/ttt_example.py
@@ -15,27 +15,27 @@ mcts = MCTS()
 # Define game
 def find_children(node):
 
-    actions = ttt.actions(node)
+    actions = ttt.actions(node[0])
     children = []
     for a in actions:
-        children.append(ttt.result(node, a))
+        children.append(ttt.result(node[0], a),)
 
     return children
 
 def random_sim(node):
     winner = None
     while winner is None:
-        actions = ttt.actions(node)
+        actions = ttt.actions(node[0])
         if len(actions) == 0:
             break
         i = randint(low=len(actions))
-        node = ttt.result(node, actions[i])
-        winner = ttt.winner(node)
+        node = (ttt.result(node[0], actions[i]),)
+        winner = ttt.winner(node[0])
 
     return node
 
 def find_reward(node):
-    winner = ttt.winner(node)
+    winner = ttt.winner(node[0])
     if winner == 'X':
         reward = 2
     if winner is None:
@@ -45,18 +45,14 @@ def find_reward(node):
 
     return reward
 
-class Node:
-    def __init__(self, board):
-        self.board = board
-
 # Assign rules of the game to mcts instance
 mcts.find_children = find_children
 mcts.random_sim = random_sim
 mcts.find_reward = find_reward
 
 # Initialise the game
-board = ttt.initial_state()
-
-# First run 
+node = (ttt.initial_state(),)
 mcts.run_itt(node)
-best_node = mcts.choose_move(node)
+##for i in range(1000):
+##    mcts.run_itt(node)
+##best_node = mcts.choose_move(node)
